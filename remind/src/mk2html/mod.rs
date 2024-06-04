5cf99ebf8c4013488543c837d7318e0121983980
jiahong.dong <dongjiahong@hotmail.com>
2024-06-04 10:42:47 +0800

    add more flags for pulldown_cmark

--- remind/src/mk2html/mod.rs
+++ remind/src/mk2html/mod.rs
@@ -3,7 +3,16 @@ use pulldown_cmark::{html, Options, Parser};
 
 pub fn markdown2html(content: &str) -> Result<String> {
     let mut options = Options::empty();
-    options.insert(Options::ENABLE_STRIKETHROUGH);
+    options.insert(
+        Options::ENABLE_STRIKETHROUGH
+            | Options::ENABLE_TABLES
+            | Options::ENABLE_MATH
+            | Options::ENABLE_FOOTNOTES
+            | Options::ENABLE_TASKLISTS
+            | Options::ENABLE_SMART_PUNCTUATION
+            | Options::ENABLE_TASKLISTS
+            | Options::ENABLE_GFM,
+    );
     let parser = Parser::new_ext(content, options);
 
     let mut html_output = String::new();
